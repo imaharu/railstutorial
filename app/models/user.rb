666b98197d557889ad56da4044340862c1e6b6b5
imaharu <light1200out@gmail.com>
2019-04-21 13:43:13 +0900

    Done 14.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
 	validates :email, format: { with: VALID_EMAIL_REGEX }
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships,	class_name: "Relationship",
+																	foreign_key: "follower_id",
+																	dependent: :destroy
+	has_many :passive_relationships,	class_name:  "Relationship",
+																		foreign_key: "followed_id",
+																		dependent:   :destroy
+
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 
 	has_secure_password
 
@@ -65,6 +75,21 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+	# ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+	end
+	
 	private
 
 	def downcase_email
