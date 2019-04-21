d05a8be4d91d8bbc90c064c9482bb754a148a101
imaharu <light1200out@gmail.com>
2019-04-21 15:02:37 +0900

    Github push

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,7 +75,8 @@ class User < ApplicationRecord
 		following_ids = "SELECT followed_id FROM relationships
                      WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+		                 OR user_id = :user_id", user_id: id)
+		# Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
 	# ユーザーをフォローする
